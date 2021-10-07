c140346e1ca95bc742a3263ca20dce7ac652804d
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-10-06 21:14:08 -0300

    jsx components

---
+++ code-snippets/jsx-components/src/index.js
@@ -0,0 +1,7 @@
+//See app here: https://codesandbox.io/s/react-components-forked-ivvr5
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
