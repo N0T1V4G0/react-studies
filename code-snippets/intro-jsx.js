09d2141756e742e10f1e8de9999161af754dfe38
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-10-06 21:00:37 -0300

    intro jsx

---
+++ code-snippets/intro-jsx.js
@@ -0,0 +1,11 @@
+// See app here: https://codesandbox.io/s/introduction-to-jsx-forked-juknh?file=/src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+    <h1>Hello, world!</h1>
+    <p>A paragraph</p>
+  </div>,
+  document.getElementById('root'),
+);
