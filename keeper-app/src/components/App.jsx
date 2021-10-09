fa5f6444749e28c9ab5c9c3e11c53faab2ef1e7f
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-10-09 19:31:33 -0300

    add Note component and finishes part 1

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Note from './Note.jsx';
 
 function App() {
   return (
     <div>
       <Header />
       <Footer />
+      <Note />
     </div>
   );
 }
