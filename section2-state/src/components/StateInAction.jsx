e3a93e20526509493c9851a33c6c5d9bae319481
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-11-30 23:40:23 -0300

    part 1 lecture 26, section 2

---
+++ section2-state/src/components/StateInAction.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class StateInAction extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>State in Action.</h1>
+      </div>
+    );
+  }
+}
+
+export default StateInAction;
