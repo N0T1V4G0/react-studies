dd97745a85ba24be3781483ccf3f15197af3b346
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-12-09 23:49:46 -0300

    finaliza StateInPractice

--- section2-state/src/components/StateInPractice.jsx
+++ section2-state/src/components/StateInPractice.jsx
@@ -5,10 +5,44 @@ class StateInPractice extends Component {
     super();
     this.state = {
       message: '',
+      imgWidth: '',
     };
   }
+
+  handleMouseEnter = () => {
+    this.setState({
+      message: '',
+    });
+  };
+
+  handleFocus = (event) => {
+    console.log(event);
+    this.setState({
+      message: 'You agree with our terms of service!',
+    });
+  };
+
+  imageLoad = (event) => {
+    console.dir(event.target); // console.dir mostra a representação DOM do target.
+    if (event.target.width > 100) {
+      console.log('Image too big!');
+    }
+  };
+
   render() {
-    return <div>StateInPractice</div>;
+    const { message } = this.state;
+
+    return (
+      <div>
+        <input onFocus={this.handleFocus} type="text" />
+        <h3 onMouseEnter={this.handleMouseEnter}>{message}</h3>
+        <img
+          src="https://miro.medium.com/max/1400/0*k9CL2yoHU6ELTkmi.png"
+          alt=""
+          onLoad={this.imageLoad}
+        />
+      </div>
+    );
   }
 }
 
