6373df4db87c7cb7efba70df58411a11a886bd65
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-12-09 23:17:09 -0300

    inicia StateInAction

--- section2-state/src/App.js
+++ section2-state/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import StateInAction from './components/StateInAction';
+import StateInPractice from './components/StateInPractice';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>Sanity Check</h1>
-        <StateInAction />
+        {/* {<StateInAction />} */}
+        {StateInPractice}
       </div>
     );
   }
---
+++ section2-state/src/components/StateInPractice.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class StateInPractice extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: '',
+    };
+  }
+  render() {
+    return <div>StateInPractice</div>;
+  }
+}
+
+export default StateInPractice;
